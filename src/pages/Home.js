8633ad042a916b5c369f430165c3a85dd5072459
Suriya <93638366+codehunter-sk@users.noreply.github.com>
2024-05-31 19:24:48 +0530

    Under construction

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <main>
+            <h2>I am Suriya Sampathkumar</h2>
+            <h4><a href="https://www.linkedin.com/in/suriyasampathkumar">Linked in profile</a></h4>
+            <p>*webpage under construction</p>
+        </main>
+    )
+}
+
+export default Home
