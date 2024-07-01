27b3ed21ca69d66b97bb59c1eb818ad2db71ccdf
Suriya <93638366+codehunter-sk@users.noreply.github.com>
2024-07-01 11:58:15 +0530

    typo updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <main>
             <h2>I am Suriya Sampathkumar</h2>
-            <h4><a href="https://www.linkedin.com/in/suriyasampathkumar">Linked in profile</a></h4>
+            <h4><a href="https://www.linkedin.com/in/suriyasampathkumar">LinkedIn profile</a></h4>
             <p>*webpage under construction</p>
         </main>
     )
