d5290cf317501d3ab4ee0ac594dc3e1b8f35940b
Suriya <93638366+codehunter-sk@users.noreply.github.com>
2024-07-06 13:01:42 +0530

    Added foodbook

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Project() {
+    return(
+        <div class='project'>
+            <h3>Project</h3>
+            <h5>Foodbook Application - <a href="https://foodbook-frontend.vercel.app/">Van Dough Pizzeria</a></h5>
+        </div>
+    )
+}
+
+export default Project;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 import './Home.css';
-import Name from '../components/Name'
+import Name from '../components/Name';
+import Project from '../components/Project';
 
 function Home() {
     return (
         <div class="home-body">
-            <h2>I am</h2>
-            <Name/>
-            <h4><a href="https://www.linkedin.com/in/suriyasampathkumar">LinkedIn profile</a></h4>
-            <p>*webpage under construction</p>
+            <div class="home-part1">
+                <h2>I am</h2>
+                <Name/>
+                <Project/>
+                <h4>For more projects - <a href="https://www.linkedin.com/in/suriyasampathkumar">LinkedIn profile</a></h4>
+            </div>
+            <div>
+                <p>*webpage under construction</p>
+            </div>
         </div>
+        
     )
 }
 
